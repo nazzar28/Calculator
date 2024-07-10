7a1dd24df5303df6efd6cb9ea8749e92baa5f2dc
nazzar28 <talantbekovnazar28@gmail.com>
2024-07-10 13:05:27 +0600

    logic added

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.calculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,31 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        EditText editTextAnswer = findViewById(R.id.editTextAnswer);
+
+        Button answerButton = findViewById(R.id.answerButton);
+
+        TextView incorrectAnswer = findViewById(R.id.textViewIncorrectAnswer);
+
+        TextView correctAnswer = findViewById(R.id.textViewCorrectAnswer);
+
+
+        answerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = editTextAnswer.getText().toString();
+                //int number = Integer.parseInt(text);
+                if(text.equals("15")){
+                    correctAnswer.setVisibility(View.VISIBLE);
+                    incorrectAnswer.setVisibility(View.GONE);
+                }else{
+                    incorrectAnswer.setVisibility(View.VISIBLE);
+                    correctAnswer.setVisibility(View.GONE);
+                }
+            }
+        });
+
+
     }
 }
